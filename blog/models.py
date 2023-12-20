c947cade47761ca1f522e0b7270a193582f51d2a
Gkicks <>
2023-12-20 21:40:52 +0000

    add posts view

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created-on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f'{self.title} | written by {self.author}'
@@ -26,7 +26,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='blog_comments')
+        Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
@@ -34,7 +34,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['created-on']
+        ordering = ["created_on"]
 
     def __str__(self):
-        return f'Comment {self.comment} by {self.author}'
+        return f'Comment {self.body} by {self.author}'
